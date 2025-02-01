c7f9e7f5cac1380391a6c20f055d917af81fce2b
Sean Boult <996134+Hacksore@users.noreply.github.com>
2025-02-01 15:28:51 -0600

    Moar stuffz again

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener("installed", function(event) {
-  console.log("Service worker install!", Date.now());
+  console.log("Service worker installed!", Date.now());
 })
 
 self.addEventListener("activate", function(event) {
-  console.log("Service worker activate!", Date.now());
+  console.log("Service worker activated!", Date.now());
 })
 
 self.addEventListener("message", function(event) {
-  console.log("Message from main page:", event.data);
-  self.registration.showNotification("Hello from the service worker!")
+  console.log("service worker:", event.data);
+  self.registration.showNotification(event.data.title)
 })
 
 self.addEventListener("notificationclick", function(event) {
